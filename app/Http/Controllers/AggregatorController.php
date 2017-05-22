bd2e08f69f8268dac3499791b761ac8f75081619
Drew Gallagher <drewg2009@gmail.com>
2017-05-22 15:39:37 -0400

    Added quote of the day to foreach

--- app/Http/Controllers/AggregatorController.php
+++ app/Http/Controllers/AggregatorController.php
@@ -29,13 +29,26 @@ class AggregatorController extends Controller
                         $this->finalString .= $history->execute(null, null, "Here is what happened on this day in history: ");
                     }
                     break;
+                case "quote":
+                    if($value){
+                        $quoteController = new QuoteController();
+                         $this->finalString .= $quoteController->execute(null, null, "Here is the quote of the day. ") . ". ";
+
+                    }
+                    break;
+
+                case "fact":
+                    if($value){
+                        $twitterController = new TwitterController();
+                        $this->finalString .= $twitterController->execute("OddFunFacts",1, "Here is the fun fact of the day from ") . ". ";
+                    }
+                    break;
                 default:
                     break;
             }
         }
 
 
-//        $quoteController = new QuoteController();
 //        $redditController = new RedditController();
 //        $twitterController = new TwitterController();
 //        $newsController = new NewsController();
@@ -44,9 +57,7 @@ class AggregatorController extends Controller
 //        $trafficController = new TrafficController("kent oh 44240", "columbus ohio", "driving");
 //
 //
-//        $this->finalString .= $quoteController->execute(null, null, "Here is the quote of the day. ") . ". ";
 //        $this->finalString .= $twitterController->execute("epochSoftware",5, "Here are the top posts from ") . ". ";
-//        //$this->finalString .= $twitterController->execute("OddFunFacts",1, "Here is the fun fact of the day from ") . ". ";
 //        $this->finalString .= $redditController->execute("sports",3, "Here are the top reddit posts from ") . ". ";
 //        $this->finalString .= $history->execute(null, null, "Here is what happened on this day in history: ");
 //        //$this->finalString .= $weather->execute(null, null, "Weather:Here is the weather for the area: ");
