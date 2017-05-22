721d3e673ff3795b4c92081cf7774a8c4c39514d
Drew Gallagher <drewg2009@gmail.com>
2017-05-22 13:23:44 -0400

    added code for getstring to weather controller. added loop for gathering module info in aggregator. Tdih is implemented

--- app/Http/Controllers/AggregatorController.php
+++ app/Http/Controllers/AggregatorController.php
@@ -12,36 +12,53 @@ class AggregatorController extends Controller
     {
     }
 
-    public function executeAll(Request $request){
+    public function executeAll(Request $request)
+    {
 
-        dd($request->moduleInfo);
+        $moduleInfoDecoded = json_decode($request->moduleInfo);
+        foreach ($moduleInfoDecoded as $key => $value) {
+            switch ($key) {
+                case "weather":
+//                    $weather = new WeatherController($moduleInfoDecoded->lat, $moduleInfoDecoded->lon, $value);
+//                    $weather->execute("",1,"");
+                    break;
+                case "tdih":
+                    if ($value) {
+                        $date = date("/m/d");
+                        $history = new HistoryController($date);
+                        $this->finalString .= $history->execute(null, null, "Here is what happened on this day in history: ");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
 
-        $quoteController = new QuoteController();
-        $redditController = new RedditController();
-        $twitterController = new TwitterController();
-        $newsController = new NewsController();
-        $countdownController = new CountdownController("March 16 2017");
-        $history = new HistoryController("/03/15");
-        $weather = new WeatherController(41.157, -81.35, false);
-        $trafficController = new TrafficController("kent oh 44240", "columbus ohio", "driving");
 
-
-        $this->finalString .= $quoteController->execute(null, null, "Here is the quote of the day. ") . ". ";
-        $this->finalString .= $twitterController->execute("epochSoftware",5, "Here are the top posts from ") . ". ";
-        //$this->finalString .= $twitterController->execute("OddFunFacts",1, "Here is the fun fact of the day from ") . ". ";
-        $this->finalString .= $redditController->execute("sports",3, "Here are the top reddit posts from ") . ". ";
-        $this->finalString .= $history->execute(null, null, "Here is what happened on this day in history: ");
-        //$this->finalString .= $weather->execute(null, null, "Weather:Here is the weather for the area: ");
-        $this->finalString .= $newsController->execute("Top", 3, "Here are the top news stories from ");
-//        $this->finalString .= $newsController->execute("Sports", 3, "Here are the top news stories from ");
-//        $this->finalString .= $newsController->execute("Technology", 3, "Here are the top news stories from ");
-//        $this->finalString .= $newsController->execute("Elections", 3, "Here are the top news stories from ");
-//        $this->finalString .= $newsController->execute("Health", 3, "Here are the top news stories from ");
-//        $this->finalString .= $newsController->execute("Business", 3, "Here are the top news stories from ");
-//        $this->finalString .= $newsController->execute("Science", 3, "Here are the top news stories from ");
-        $this->finalString .= $countdownController->execute("My Birthday", null, "Countdown: ");
-        $this->finalString .= $trafficController->execute(null, null, "It will take you ");
-
+//        $quoteController = new QuoteController();
+//        $redditController = new RedditController();
+//        $twitterController = new TwitterController();
+//        $newsController = new NewsController();
+//        $countdownController = new CountdownController("March 16 2017");
+//
+//        $trafficController = new TrafficController("kent oh 44240", "columbus ohio", "driving");
+//
+//
+//        $this->finalString .= $quoteController->execute(null, null, "Here is the quote of the day. ") . ". ";
+//        $this->finalString .= $twitterController->execute("epochSoftware",5, "Here are the top posts from ") . ". ";
+//        //$this->finalString .= $twitterController->execute("OddFunFacts",1, "Here is the fun fact of the day from ") . ". ";
+//        $this->finalString .= $redditController->execute("sports",3, "Here are the top reddit posts from ") . ". ";
+//        $this->finalString .= $history->execute(null, null, "Here is what happened on this day in history: ");
+//        //$this->finalString .= $weather->execute(null, null, "Weather:Here is the weather for the area: ");
+//        $this->finalString .= $newsController->execute("Top", 3, "Here are the top news stories from ");
+////        $this->finalString .= $newsController->execute("Sports", 3, "Here are the top news stories from ");
+////        $this->finalString .= $newsController->execute("Technology", 3, "Here are the top news stories from ");
+////        $this->finalString .= $newsController->execute("Elections", 3, "Here are the top news stories from ");
+////        $this->finalString .= $newsController->execute("Health", 3, "Here are the top news stories from ");
+////        $this->finalString .= $newsController->execute("Business", 3, "Here are the top news stories from ");
+////        $this->finalString .= $newsController->execute("Science", 3, "Here are the top news stories from ");
+//        $this->finalString .= $countdownController->execute("My Birthday", null, "Countdown: ");
+//        $this->finalString .= $trafficController->execute(null, null, "It will take you ");
 
 
         echo $this->finalString;
--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -6,20 +6,20 @@ class WeatherController extends Controller implements ModuleInterface
 {
     private $latitude = "";
     private $longitude = "";
-    private $isCelsius = "";
+    private $info;
     private $finalUrl1;
     private $finalUrl2;
     private $finalUrl3;
 
 
-    public function __construct($latitude, $longitude, $isCelisus)
+    public function __construct($lat, $lon, $info)
     {
-        $this->latitude = $latitude;
-        $this->longitude = $longitude;
-        $this->isCelsius = $isCelisus;
-        $this->finalUrl1 = "http://api.openweathermap.org/data/2.5/weather?lat=" . $this->latitude . "&lon=" . $this->longitude . "&APPID=fa18ccaa8bfec044e6bf94d88d7055dd&units=" . (($this->isCelsius) ? "metric" : "imperial");
-        $this->finalUrl2 = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" . $this->latitude . "" . "&lon=" . $this->longitude . "&APPID=fa18ccaa8bfec044e6bf94d88d7055dd&cnt=1&mode=json" . "&units=" . (($this->isCelsius) ? "metric" : "imperial");
-        $this->finalUrl3 = "http://api.openweathermap.org/data/2.5/forecast?lat=" . $this->latitude . "" . "&lon=" . $this->longitude . "&APPID=fa18ccaa8bfec044e6bf94d88d7055dd&units=" . (($this->isCelsius) ? "metric" : "imperial");
+        $this->latitude = $lat;
+        $this->longitude = $lon;
+        $this->info = $info;
+        $this->finalUrl1 = "http://api.openweathermap.org/data/2.5/weather?lat=" . $this->latitude . "&lon=" . $this->longitude . "&APPID=fa18ccaa8bfec044e6bf94d88d7055dd&units=" . (($this->info->fahrenheit) ? "imperial" : "metric");
+        $this->finalUrl2 = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" . $this->latitude . "" . "&lon=" . $this->longitude . "&APPID=fa18ccaa8bfec044e6bf94d88d7055dd&cnt=1&mode=json" . "&units=" . (($this->info->fahrenheit) ? "imperial" : "metric");
+        $this->finalUrl3 = "http://api.openweathermap.org/data/2.5/forecast?lat=" . $this->latitude . "" . "&lon=" . $this->longitude . "&APPID=fa18ccaa8bfec044e6bf94d88d7055dd&units=" . (($this->info->fahrenheit) ? "imperial" : "metric");
 
     }
 
@@ -27,15 +27,28 @@ class WeatherController extends Controller implements ModuleInterface
     public function execute($name, $limit, $message)
     {
         $string = "";
-        $response  = file_get_contents($this->finalUrl1);
-        $jsonobj  = json_decode($response);
-        $string  = $this->getString();
-        dd($jsonobj);
+        $response  = file_get_contents($this->finalUrl2);
+        $jsonObj  = json_decode($response);
+//        dd($jsonObj);
+        $string  = $this->getString($jsonObj);
         return $string;
     }
 
-    private function getString($array)
+    private function getString($jsonObj)
     {
+        $string = "Here is the weather for the " . $jsonObj->city->name . " area";
+        if($this->info->description){
+
+        }
+        if($this->info->currentTemp) {
+            $string .= "It is currently " . currentTemp . " degrees. ";
+        }
+//        if(getMaxTemp) sb.append("The high today will be " + (int)maxTemp + " degrees. ");
+//        if(rain > 0 && snow > 0) sb.append("Expect rain and snow today. ");
+//        else if (rain > 0) sb.append("Expect rain today. ");
+//        else if (snow > 0) sb.append("Expect snow today. ");
+
+
 //    {
 //    sb.append("Weather:Here is the weather for the " + locationName + " area. ");
 //        if(getDescription) sb.append(description + ". ");
