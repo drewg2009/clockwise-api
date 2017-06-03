5bc81d434ee2808a85054243049d2c733e91b8b5
Drew Gallagher <drewg2009@gmail.com>
2017-06-03 17:21:51 -0400

    removed format string and fixed query build string

--- app/Http/Controllers/GooglePlacesController.php
+++ app/Http/Controllers/GooglePlacesController.php
@@ -11,24 +11,7 @@ class GooglePlacesController extends Controller
         $placesApiKey = "AIzaSyCCb7yaagbE_5Gujv9U5AQOYlVXF-R-QMo";
         $placesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
         $textSearchUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
-        $result = file_get_contents($textSearchUrl . $this->formatLocationString($request->placesQuery,false) . "&key=" . $placesApiKey);
+        $result = file_get_contents($textSearchUrl . $request->placesQuery . "&key=" . $placesApiKey);
         echo $result;
-    }
-
-    private function formatLocationString($string, $useLatLon)
-    {
-        $newString = "";
-
-        if ($useLatLon) {
-            //$newString .= $this->lat . "," . $this->lon;
-        } else {
-            $array = explode(" ", $string);
-            for ($i = 0; $i < sizeof($array); $i++) {
-                if ($i == sizeof($array) - 1) $newString .= $array[$i];
-                else $newString .= $array[$i] . "+";
-            }
-
-        }
-        return $newString;
     }
 }
