6eb71fef5a86d8ae3a24cdbfabf8bd9c6e29a59d
Drew Gallagher <drewg2009@gmail.com>
2017-06-03 18:03:57 -0400

    Added json_encode to google places controller echo

--- app/Http/Controllers/GooglePlacesController.php
+++ app/Http/Controllers/GooglePlacesController.php
@@ -9,9 +9,8 @@ class GooglePlacesController extends Controller
     public function getNearbyLocations(Request $request)
     {
         $placesApiKey = "AIzaSyCCb7yaagbE_5Gujv9U5AQOYlVXF-R-QMo";
-        $placesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
         $textSearchUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
         $result = file_get_contents($textSearchUrl . $request->placesQuery . "&key=" . $placesApiKey);
-        echo $result;
+        echo json_encode($result);
     }
 }
