34583fc7ac79a910bf55eb86d72447a86295c16a
Drew Gallagher <drewg2009@gmail.com>
2017-06-03 21:28:26 -0400

    Added back string formatting and fixed request

--- app/Http/Controllers/GooglePlacesController.php
+++ app/Http/Controllers/GooglePlacesController.php
@@ -10,7 +10,19 @@ class GooglePlacesController extends Controller
     {
         $placesApiKey = "AIzaSyCCb7yaagbE_5Gujv9U5AQOYlVXF-R-QMo";
         $textSearchUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
-        $result = file_get_contents($textSearchUrl . $request->placesQuery . "&key=" . $placesApiKey);
+        $result = file_get_contents($textSearchUrl . $this->formatLocationString($request->placesQuery) . "&key=" . $placesApiKey);
         echo json_encode($result);
+    }
+
+    private function formatLocationString($string)
+    {
+        $newString = "";
+        $array = explode(" ", $string);
+        for ($i = 0; $i < sizeof($array); $i++) {
+            if ($i == sizeof($array) - 1) $newString .= $array[$i];
+            else $newString .= $array[$i] . "+";
+        }
+
+        return $newString;
     }
 }
